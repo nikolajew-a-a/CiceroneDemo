1b9261417a9284fd453e36938412907df7f55f84
an.nikolaev <an.nikolaev@okko.tv>
2022-09-21 21:49:50 +0300

    ViewModel delegate added

--- app/src/main/java/com/example/android/ciceronedemo/ui/selection/SelectionFragment.kt
+++ app/src/main/java/com/example/android/ciceronedemo/ui/selection/SelectionFragment.kt
@@ -1,11 +1,9 @@
 package com.example.android.ciceronedemo.ui.selection
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.android.ciceronedemo.R
 import com.example.android.ciceronedemo.databinding.SelectionFragmentBinding
@@ -13,12 +11,10 @@ import com.example.android.ciceronedemo.databinding.SelectionFragmentBinding
 class SelectionFragment : Fragment(R.layout.selection_fragment) {
 
     private val viewBinding by viewBinding(SelectionFragmentBinding::bind)
-    private lateinit var viewModel: SelectionViewModel
-
+    private val viewModel by viewModels<SelectionViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[SelectionViewModel::class.java]
 
         bindViews()
         initButtons()
