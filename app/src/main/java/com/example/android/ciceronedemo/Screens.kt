9e51298f1a1513f8083d6cf68f113ce0ca3c70d5
an.nikolaev <an.nikolaev@okko.tv>
2022-09-25 01:41:19 +0300

    Implemented navigation with Cicerone in activity

--- app/src/main/java/com/example/android/ciceronedemo/Screens.kt
+++ app/src/main/java/com/example/android/ciceronedemo/Screens.kt
@@ -6,6 +6,8 @@ import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object Screens {
 
+    fun SelectionFragment() = FragmentScreen { com.example.android.ciceronedemo.ui.selection.SelectionFragment.newInstance() }
+
     fun SimpleNavigationFragment(number: Int = 0) =
         FragmentScreen { SimpleNavigationFragment.newInstance(SimpleNavigationArgs(number = number, time = System.currentTimeMillis())) }
 }
--- app/src/main/java/com/example/android/ciceronedemo/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/android/ciceronedemo/ui/base/BaseActivity.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.android.ciceronedemo.App
 import com.example.android.ciceronedemo.R
+import com.example.android.ciceronedemo.Screens
 import com.example.android.ciceronedemo.databinding.BaseActivityBinding
 import com.example.android.ciceronedemo.ui.selection.SelectionFragment
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
 import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import java.lang.ref.WeakReference
 import java.util.ArrayList
@@ -22,7 +24,11 @@ class BaseActivity : AppCompatActivity(), ChainHolder {
     override val chain = ArrayList<WeakReference<Fragment>>()
 
     @Inject
+    lateinit var router: Router
+
+    @Inject
     lateinit var navigatorHolder: NavigatorHolder
+
     private val navigator: Navigator = object : AppNavigator(this, R.id.container) {
         override fun applyCommands(commands: Array<out Command>) {
             super.applyCommands(commands)
@@ -37,11 +43,8 @@ class BaseActivity : AppCompatActivity(), ChainHolder {
         App.INSTANCE.appComponent.inject(this)
         setContentView(R.layout.base_activity)
 
-        //TODO (replace with Cicerone)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SelectionFragment.newInstance())
-                .commitNow()
+            router.replaceScreen(Screens.SelectionFragment())
         }
     }
 
